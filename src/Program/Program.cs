10de58aabe67164343ce98f97840c23fe44e43a2
Joaco Balian <joaquin.balian@correo.ucu.edu.uy>
2024-08-08 19:44:26 -0300

    Agregar resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,17 @@
-﻿public class Substraction
+﻿Console.WriteLine(Addition.Add(7, 2));
+Console.WriteLine(Substraction.Substract(10, 1));
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2));
+
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class Substraction
 {
     public static int Substract(int a, int b)
     {
@@ -11,5 +24,13 @@ public class Multiplication
     public static int Multiply(int a, int b)
     {
         return a * b;
+    }
+}
+
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
     }
 }
