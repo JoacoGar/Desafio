e411926fdc2669b4f090d061ff2de244c2729162
Joaco Balian <joaquin.balian@correo.ucu.edu.uy>
2024-08-12 11:12:26 -0300

    Multiplicacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,14 +2,6 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
-//Cambio de prueba
-public class Addition
-{
-    public static int Add(int a, int b)
-    {
-        return a + b;
-    }
-}
 
 public class Multiplication
 {
@@ -17,5 +9,4 @@ public class Multiplication
     {
         return a * b;
     }
-}
 }
